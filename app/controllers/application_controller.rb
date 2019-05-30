3dfc598acca13da2c1df4448d191dec6514f61cd
Priscilla H <priscillamhuynh@gmail.com>
2019-05-30 06:35:20 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,21 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
+  get '/articles' do
+    erb :index
+  end
+  
+  get '/articles/new' do
+    erb :new
+  end
+  
+  post '/articles' do
+    @article=Article.create(params[:article])
+    redirect to '/articles/#{@article.id}'
+  end
+  
+  get '/articles' do 
+    @articles=Article.all 
+    erb :index
   end
 end
